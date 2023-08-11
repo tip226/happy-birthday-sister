2d54c6beedd8b644760b8c728cb91ff1a560990a
Tina Pham <tip226@lehigh.edu>
2023-08-12 00:40:25 +0700

    videos work

--- js/scripts.js
+++ js/scripts.js
@@ -127,33 +127,46 @@ function createBalloons(words) {
     });
 }
 
-const videos = document.querySelectorAll('.video-item video');
+function handleVideoEnd(videoElement) {
+    // Resume the timeline animation
+    const timeline = document.querySelector('.photo-timeline');
+    timeline.style.animationPlayState = 'running';
+    timeline.style.webkitAnimationPlayState = 'running';
+}
 
-videos.forEach(video => {
-    video.addEventListener('play', function() {
-        // Pause the timeline animation when a video starts playing
-        document.querySelector('.photo-timeline').style.animationPlayState = 'paused';
+// Function to handle video visibility using Intersection Observer
+function handleVideoVisibility(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && entry.target.paused) {
+            // Pause the timeline animation
+            const timeline = document.querySelector('.photo-timeline');
+            const computedStyle = getComputedStyle(timeline);
+            const animationPlayState = computedStyle.animationPlayState || computedStyle.webkitAnimationPlayState;
 
-        // When the video ends, resume the timeline animation
-        video.addEventListener('ended', function() {
-            document.querySelector('.photo-timeline').style.animationPlayState = 'running';
-        });
+            if (animationPlayState === 'running') {
+                timeline.style.animationPlayState = 'paused';
+                timeline.style.webkitAnimationPlayState = 'paused';
+            }
+
+            // Play the video
+            entry.target.play();
+        } else if (!entry.isIntersecting && !entry.target.paused) {
+            entry.target.pause();
+        }
     });
-});
+}
 
-// Adjust the timeline animation duration based on video duration
-const adjustTimelineDuration = () => {
-    let totalDuration = 20; // default duration
-    videos.forEach(video => {
-        video.addEventListener('loadedmetadata', function() {
-            totalDuration += video.duration;
-        });
-    });
-
-    document.querySelector('.photo-timeline').style.animationDuration = totalDuration + 's';
+// Initialize Intersection Observer
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
 };
 
-adjustTimelineDuration();
+const observer = new IntersectionObserver(handleVideoVisibility, options);
+const videos = document.querySelectorAll('video');
+videos.forEach(video => observer.observe(video));
+
 
 
 cake.addEventListener('click', () => {
